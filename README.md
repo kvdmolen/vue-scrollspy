50cad765d89eb3292d431b6b256e087e477e2955
Klaas van der Molen <kvdmolen@KlaasPro.local>
2016-04-08 11:01:01 +0200

    initial

---
+++ README.md
@@ -0,0 +1,45 @@
+# vue-scrollspy
+
+## Installation
+
+```
+$ npm install vue-scrollspy --save
+```
+
+## Setup
+
+```js
+var Vue = require('vue');
+var Scrollspy = require('vue-scrollspy');
+Vue.use(Scrollspy)
+```
+
+## Usage
+
+```html
+<ul>
+    <li :class="{active:scrollPos==0}"><a @click="$scrollTo(0)">Menu 1</a></li>
+    <li :class="{active:scrollPos==0}"><a @click="$scrollTo(1)">Menu 2</a></li>
+</ul>
+
+<div v-scrollspy :steps="30" :time="200">
+    <div>
+        <h1>Header 1</h1>
+        <p>Content</p>
+    </div>
+    <div>
+        <h1>Header 2</h1>
+        <p>Content</p>
+    </div>
+</div>
+```
+
+When the DOM is ready, or when the content has changed, call `$scrollSet()`:
+
+```js
+new Vue({
+    ready: function(){
+        this.$scrollSet()
+    }
+})
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+﻿; (function () {
+    var vueScrollspy = {};
+    vueScrollspy.install = function (Vue) {
+
+        Vue.scrollPos = 0
+
+        Vue.directive('scrollspy', {
+            params: ["steps","time"],
+            scrollSections: [0],
+            scroll: function(){
+                var pos = this.el.scrollTop
+                var i = 0
+                while(pos >= this.scrollSections[i]){i++}
+                this.vm.$set("scrollPos",i ? i - 1 : 0)
+            },
+            scrollTo: function(id){
+                var current = this.el.scrollTop
+                var target = this.scrollSections[id]
+                var gap = target - current
+                var timems = parseInt(parseInt(this.params.time) / parseInt(this.params.steps))
+                var steps = this.params.steps
+                var el = this.el
+
+                for(var i = 1; i <= steps; i++){
+                    (function(){
+                        var pos = current + (gap/steps)*i;
+                        setTimeout(function(){el.scrollTop = pos}, timems*i)
+                    })();
+                }
+            },
+            init: function(){
+                var sections = this.el.children
+                for (var i = 0; i < sections.length; i++){
+                    if(sections[i].offsetTop > 0){
+                        this.scrollSections.push(sections[i].offsetTop)
+                    }
+                }
+            },
+            bind: function(){
+                this.vm.$set("scrollPos", 0)
+
+                this.el.addEventListener('scroll', this.scroll.bind(this))
+
+                Vue.prototype.$scrollTo = this.scrollTo.bind(this)
+                Vue.prototype.$scrollSet = this.init.bind(this)
+
+            },
+            update: function(data) {
+            },
+            unbind: function() {
+                this.el.removeEventListener('scroll', scroll)
+            },
+
+        })
+    }
+
+    if (typeof exports == "object") {
+        module.exports = vueScrollspy;
+    } else if (typeof define == "function" && define.amd) {
+        define([], function () { return vueScrollspy });
+    } else if (window.Vue) {
+        window.vueScrollspy = vueScrollspy;
+        Vue.use(vueScrollspy);
+    }
+
+})();
